3a6dccacd038b0aac2c742085096725a96945e56
Haritha Thilakarathne <harithalht@gmail.com>
2021-10-17 18:13:03 +1100

    add debugs

--- vedatad/misc/segment/segment.py
+++ vedatad/misc/segment/segment.py
@@ -164,14 +164,31 @@ def temporal_distance(segments1, segments2, is_aligned=False):
         return segments1.new(rows, 1) if is_aligned else segments2.new(
             rows, cols)
 
+    # #initial way
+    # if is_aligned:
+    #     segments1_midpoints = torch.mean(segments1, dim=1)
+    #     segments2_midpoints = torch.mean(segments2, dim=1)
+    #     abs_distance = torch.abs(segments1_midpoints.unsqueeze(1) - segments2_midpoints)
+    # else:
+    #     segments1_midpoints = torch.mean(segments1, dim=1)
+    #     segments2_midpoints = torch.mean(segments2, dim=1)
+    #     abs_distance = torch.abs(segments1_midpoints - segments2_midpoints)
+
+    #next way
     if is_aligned:
         segments1_midpoints = torch.mean(segments1, dim=1)
         segments2_midpoints = torch.mean(segments2, dim=1)
-        abs_distance = torch.abs(segments1_midpoints.unsqueeze(1) - segments2_midpoints)
+        print('is aligned section')
+        print('seg1 - ', segments1_midpoints.shape)
+        print('seg2 - ', segments2_midpoints.shape)
+        abs_distance = torch.abs(segments1_midpoints - segments2_midpoints)
     else:
         segments1_midpoints = torch.mean(segments1, dim=1)
         segments2_midpoints = torch.mean(segments2, dim=1)
-        abs_distance = torch.abs(segments1_midpoints - segments2_midpoints)
+        print('not aligned section')
+        print('seg1 - ', segments1_midpoints.shape)
+        print('seg2 - ', segments2_midpoints.shape)
+        abs_distance = torch.abs(segments1_midpoints.unsqueeze(1) - segments2_midpoints)
 
     if is_numpy:
         abs_distance = abs_distance.numpy()
