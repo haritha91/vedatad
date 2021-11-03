d452f241a57b1152916ad26d39bb4a9db6137bf8
Haritha Thilakarathne <harithalht@gmail.com>
2021-11-04 03:14:42 +1100

    add delta transition coder

--- vedatad/misc/segment/coders/__init__.py
+++ vedatad/misc/segment/coders/__init__.py
@@ -1,5 +1,6 @@
 from .base_segment_coder import BaseSegmentCoder
 from .delta_segment_coder import DeltaSegmentCoder
 from .pseudo_segment_coder import PseudoSegmentCoder
+from .delta_transition_coder import DeltaTransitionCoder
 
-__all__ = ['BaseSegmentCoder', 'PseudoSegmentCoder', 'DeltaSegmentCoder']
+__all__ = ['BaseSegmentCoder', 'PseudoSegmentCoder', 'DeltaSegmentCoder', 'DeltaTransitionCoder']
