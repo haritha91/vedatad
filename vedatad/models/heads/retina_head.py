0a0cdbfb38cc8495fe979724d868cfac4640f362
Haritha Thilakarathne <harithalht@gmail.com>
2021-11-07 23:20:25 +1100

    change for single point

--- vedatad/models/heads/retina_head.py
+++ vedatad/models/heads/retina_head.py
@@ -71,6 +71,9 @@ class RetinaHead(AnchorHead):
             self.num_anchors * self.cls_out_channels,
             3,
             padding=1)
+        # self.retina_reg = nn.Conv1d(
+        #     self.feat_channels, self.num_anchors * 2, 3, padding=1)
+        # Single point prediction
         self.retina_reg = nn.Conv1d(
             self.feat_channels, self.num_anchors * 2, 3, padding=1)
 
