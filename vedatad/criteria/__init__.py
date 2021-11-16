ed63c972ae315bbf468874d260db4a6608f3db22
Haritha Thilakarathne <harithalht@gmail.com>
2021-11-16 14:27:14 +1100

    remove transition

--- vedatad/criteria/__init__.py
+++ vedatad/criteria/__init__.py
@@ -1,5 +1,4 @@
 from .builder import build_criterion
 from .segment_anchor_criterion import SegmentAnchorCriterion
-from .transition_anchor_criterion import TransitionAnchorCriterion
 
-__all__ = ['SegmentAnchorCriterion', 'build_criterion', 'TransitionAnchorCriterion']
+__all__ = ['SegmentAnchorCriterion', 'build_criterion']
