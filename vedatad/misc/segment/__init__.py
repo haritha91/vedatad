c11275b68a970aa67bb2b2919470d73fc44d7201
haritha91 <harithalht@gmail.com>
2021-11-22 11:59:16 +1100

    change for deltapointcoder

--- vedatad/misc/segment/__init__.py
+++ vedatad/misc/segment/__init__.py
@@ -1,6 +1,6 @@
 from .assigners import MaxIoUAssigner
 from .builder import build_assigner, build_sampler, build_segment_coder
-from .coders import BaseSegmentCoder, DeltaSegmentCoder, PseudoSegmentCoder
+from .coders import BaseSegmentCoder, DeltaSegmentCoder, PseudoSegmentCoder, DeltaPointCoder
 from .samplers import (CombinedSampler, InstanceBalancedPosSampler,
                        IoUBalancedNegSampler, PseudoSampler, RandomSampler)
 from .segment import (distance2segment, multiclass_nms, segment2result,
@@ -9,7 +9,7 @@ from .segment import (distance2segment, multiclass_nms, segment2result,
 __all__ = [
     'MaxIoUAssigner', 'segment2result', 'segment_overlaps', 'temporal_distance','distance2segment',
     'multiclass_nms', 'build_assigner', 'build_segment_coder', 'build_sampler',
-    'BaseSegmentCoder', 'DeltaSegmentCoder', 'PseudoSegmentCoder',
+    'BaseSegmentCoder', 'DeltaSegmentCoder', 'PseudoSegmentCoder', 'DeltaPointCoder',
     'PseudoSampler', 'CombinedSampler', 'RandomSampler',
     'InstanceBalancedPosSampler', 'IoUBalancedNegSampler'
 ]
